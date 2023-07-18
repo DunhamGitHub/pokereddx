ad07cd93989535c1149e281f03b65a91481e2d67
Silvan <sp@midix.com>
2023-07-18 11:02:13 +0200

    Route#1 can catch 3 Starters and Mew

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,16 +1,16 @@
 Route1Mons:
 ; Grass Mons
 	db $19
-	db 3,GOLDUCK
-	db 3,WEEDLE
-	db 3,EXEGGUTOR
-	db 2,KANGASKHAN
-	db 2,WEEPINBELL
-	db 3,RAPIDASH
-	db 3,BEEDRILL
-	db 4,SHELLDER
-	db 4,CLEFFA
-	db 5,BEEDRILL
+	db 1,BULBASAUR
+	db 1,BULBASAUR
+	db 1,CHARMANDER
+	db 1,CHARMANDER
+	db 1,SQUIRTLE
+	db 1,SQUIRTLE
+	db 1,MEW
+	db 1,MEW
+	db 1,MEW
+	db 1,MEW
 
 ; Water Mons
 	db $00
